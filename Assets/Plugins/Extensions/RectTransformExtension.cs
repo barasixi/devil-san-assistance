f02b826c7d3374bb7cd6a09b65a0cfe4fc70893e
Nozomi Tanaka <barasixi@gmail.com>
2018-11-19 14:05:43 +0900

    Stretch強制はExtensionにした

---
+++ Assets/Plugins/Extensions/RectTransformExtension.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class RectTransformExtension 
+{
+  public static void SetStretch(this RectTransform rect)
+  {
+    rect.anchorMin = Vector2.zero;
+    rect.anchorMax = Vector2.one;
+    rect.sizeDelta = Vector2.zero;
+  }
+}
